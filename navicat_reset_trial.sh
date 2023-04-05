ecf40bd7a29535646e26c39cf4f8188ed312b1f8
yin.zhang <yin.zhang@mihoyo.com>
2023-04-06 01:02:53 +0800

    add reset navicat trial script

---
+++ navicat_reset_trial.sh
@@ -0,0 +1,4 @@
+/usr/libexec/PlistBuddy -c "Delete :B150F41ABDCF9BC7F3B1148FF6936E69" ~/Library/Preferences/com.navicat.NavicatPremium.plist
+cd ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/
+rm -rf .5CFBB933F40362983EDB94CB34428E6F
+echo "Done"
